6e0fb12605a8f3512fae59a05d920347c4fb4c18
Rahavee <rmanian@uvm.edu>
2020-04-11 12:01:43 -0400

    allowabale moves are shown

--- Button.py
+++ Button.py
@@ -4,16 +4,16 @@ import pygame
 class Button:
 
     # the constructor for the button class, with optional label
-    def __init__(self, screen, color, x, y, width, height, label=""):
+    def __init__(self, screen, color, x, y, width, height, label="", widthLine=0):
         self.color = color
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.label = label
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), widthLine)
         if self.label != '':
-            font = pygame.font.SysFont('comicsans', 60)
+            font = pygame.font.SysFont('comicsans', 30)
             text = font.render(self.label, 1, (0, 0, 0))
             screen.blit(text, (
                 self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
--- GUI.py
+++ GUI.py
@@ -1,22 +1,27 @@
 import pygame
 from Button import Button
+from board import Board
 
 # global variables
+game = Board(2, 1)
 tiles = []
+otherButtons = []
 bgOptions = []
 running = True
-gameArray = [[0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 2, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 2, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
-green = (0, 255, 0)
-blue = (0, 0, 255)
-red = (255, 0, 0)
-purple = (125, 0, 125)
+gameArray = []
+green = (34, 139, 34)
+blue = (34, 34, 139)
+red = (139, 34, 34)
+purple = (128, 0, 128)
 background = green
+widthLine = 0
+flag = False
 
 
 # function to get the array from the backend
 def updateGameArray():
     global gameArray
+    gameArray = game.get_current_layout()
     # TODO: get the updated array from the backend when undo-ing a move or to see computer's move
     print(" Getting the array from the backend ")
 
@@ -31,6 +36,7 @@ def getGameArray():
 # function to draw the 8x8 board and the pieces
 def drawBoard(screen):
     global tiles
+    updateGameArray()
     tiles = []
     # To draw the grid squares
     x = 20
@@ -38,7 +44,14 @@ def drawBoard(screen):
     square = 90
     for i in range(0, 8):
         for j in range(0, 8):
-            tiles.append(Button(screen, background, x, y, square, square))
+            if gameArray[i][j] == 3:
+                color = background
+                if flag:
+                    color = (0, 0, 0)
+
+                tiles.append(Button(screen, color, x, y, square, square, widthLine=widthLine))
+            else:
+                tiles.append(Button(screen, background, x, y, square, square))
             x = x + square + 1
         x = 20
         y = y + square + 1
@@ -53,6 +66,7 @@ def drawBoard(screen):
                 pygame.draw.circle(screen, (255, 255, 255), (xCir, yCir), radius)
             if gameArray[i][j] == 2:
                 pygame.draw.circle(screen, (0, 0, 0), (xCir, yCir), radius)
+
             xCir = xCir + square + 1
         xCir = 65
         yCir = yCir + square + 1
@@ -61,7 +75,7 @@ def drawBoard(screen):
 # buttons for changing the background color
 def changeBackground(screen):
     global bgOptions
-    bgOptions=[]
+    bgOptions = []
     Button(screen, (255, 255, 255), 900, 300, 100, 100, "background color")
     bgOptions.append(Button(screen, red, 800, 400, 30, 30))
     bgOptions.append(Button(screen, blue, 840, 400, 30, 30))
@@ -69,8 +83,14 @@ def changeBackground(screen):
     bgOptions.append(Button(screen, green, 920, 400, 30, 30))
 
 
+def displayOtherButtons(screen):
+    global otherButtons
+    otherButtons = []
+    otherButtons.append(Button(screen, (210, 210, 210), 900, 100, 100, 100, "Show moves"))
+
+
 def eventListener(position):
-    global running, gameArray, background
+    global running, gameArray, background, widthLine, flag
     for event in pygame.event.get():
         # Did the user click the window close button?
         if event.type == pygame.QUIT:
@@ -82,12 +102,23 @@ def eventListener(position):
                     row = t // 8
                     column = t % 8
                     # TODO: Send the row and column of where the user wants to place new tile and check if that is valid
-                    # TODO: Get who is playing from the backend and pick the color of tile accordingly
-                    gameArray[row][column] = 1
-                    # TODO: If playing against computer then call updateGameArray() here
+                    if game.place_piece(row, column, game.get_current_turn()):
+                        # TODO: Get who is playing from the backend and pick the color of tile accordingly
+                        gameArray[row][column] = game.get_current_turn()
+                    # TODO: Flip the pieces
+                    updateGameArray()
+                    # TODO: If playing against computer then call updateGameArray() here.
             for c in range(len(bgOptions)):
                 if bgOptions[c].isOver(position):
                     background = bgOptions[c].color
+            for b in range(len(otherButtons)):
+                if otherButtons[b].isOver(position):
+                    if flag:
+                        widthLine = 0
+                        flag = False
+                    else:
+                        widthLine = 3
+                        flag = True
 
 
 def mainGameLoop():
@@ -103,6 +134,7 @@ def mainGameLoop():
 
         # Draw the screen elements
         drawBoard(screen)
+        displayOtherButtons(screen)
         changeBackground(screen)
         pygame.display.update()
 
